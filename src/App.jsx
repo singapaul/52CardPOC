66fabc4d5a5e9bb0dd8f647153e0c182330e2df3
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2023-04-05 19:55:14 +0100

    Set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>52 card pick up POC</h1>
+      <p className="text-red-500 mt-8">Hello world!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -17,9 +16,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p> */}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

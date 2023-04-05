54ec970f5f4284c409db205538d23928235c4b2d
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2023-04-05 19:27:21 +0100

    Initialise repo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>52 card pick up POC</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        {/* <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
+      </div>
+
+    </div>
+  )
+}
+
+export default App

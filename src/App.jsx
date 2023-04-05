e6a2f913d110d6a39b3912e2a870b2a84b1a45a7
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2023-04-05 23:52:06 +0100

    Initiated deck

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import Card from "./Components/Card/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>52 card pick up POC</h1>
+      <h1>26 pairs POC</h1>
       <p className="text-red-500 mt-8">Hello world!</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Card/>
         {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p> */}
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = ({ suit, val }) => {
+  return (
+    <div className="h-10 w-10 p-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      Card
+    </div>
+  );
+};
+
+export default Card;
